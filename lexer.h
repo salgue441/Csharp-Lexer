cd8d78951a91e1e250562dc9f989edc453d1f0c0
salgue441 <carlossalguero441@gmail.com>
2023-04-17 16:43:10 -0600

    initial state of the project

---
+++ lexer.h
@@ -0,0 +1,22 @@
+/**
+ * @file lexer.h
+ * @author Carlos Salguero
+ * @author Sergio Garnica
+ * @brief Implementation of the lexer for Csharp language
+ * @version 0.1
+ * @date 2023-04-17
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#ifdef _WIN32
+#include <windows.h>
+#else
+#include <unistd.h>
+#endif
+
+#ifndef LEXER_H
+#define LEXER_H
+
+#endif //! LEXER_H
---
+++ main.cpp
@@ -0,0 +1,45 @@
+/**
+ * @file main.cpp
+ * @author Carlos Salguero
+ * @author Sergio Garnica
+ * @brief Parallel lexer for Csharp language
+ * @version 0.1
+ * @date 2023-04-17
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <pthread.h>
+#include <mutex>
+#include <chrono>
+#include <unistd.h>
+#include <sys/types.h>
+
+#include "lexer.h"
+
+// Constants & Macros
+#define NUM_THREADS 8
+
+// Function prototypes
+
+// Threads structs
+struct thread_data
+{
+};
+
+// Main function
+/**
+ * @brief
+ * Main function of the program
+ * @param argc - Number of arguments
+ * @param argv - Arguments
+ * @return int - 0 if success, 1 if error
+ */
+int main(int argc, char **argv) {}
+
+// Function definitions
