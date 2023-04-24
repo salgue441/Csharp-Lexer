0baaedc2cbcbc73d8eda416ec0cd364af017126c
salgue441 <carlossalguero441@gmail.com>
2023-04-23 18:39:42 -0600

    Implemented new version of lexer.h class

--- lexer.h
+++ lexer.h
@@ -3,7 +3,7 @@
  * @author Carlos Salguero
  * @author Sergio Garnica
  * @brief Lexer class definition
- * @version 0.1
+ * @version 0.3
  * @date 2023-04-17
  *
  * @copyright Copyright (c) 2023
@@ -20,7 +20,7 @@
 #include <mutex>              // for std::mutex, std::lock_guard
 #include <condition_variable> // for std::condition_variable
 #include <optional>           // for std::optional
-#include <pthread.h>          // for pthread_t
+#include <thread>             // for std::thread and std::this_thread
 #include <future>             // for std::async, std::future
 #include <memory>             // for smart pointers
 
@@ -30,31 +30,41 @@
 class Lexer
 {
 public:
+    // Constructor
     Lexer() = default;
+
+    // Destructor
     ~Lexer() = default;
 
+    // Access methods
     std::string get_html_classes() const;
     std::string get_html_title() const;
-    std::vector<std::unique_ptr<Token>> get_tokens() const;
+    std::vector<std::shared_ptr<Token>> get_tokens() const;
     bool is_finished() const;
 
-    void set_html_classes(const std::string_view &);
-    void set_html_title(const std::string_view &);
+    // Mutator methods
+    void set_html_classes(const std::string &);
+    void set_html_title(const std::string &);
+    void set_tokens(const std::vector<std::shared_ptr<Token>> &);
 
+    // Functions
     void tokenize(const std::string_view &);
 
 private:
     std::string m_html_classes;
     std::string m_html_title;
-    std::vector<std::unique_ptr<Token>> m_tokens;
+    std::vector<std::shared_ptr<Token>> m_tokens;
     std::mutex m_mutex;
     std::condition_variable m_cv;
     std::size_t m_max_threads{std::thread::hardware_concurrency()};
-    bool m_finished{false};
-    std::vector<pthread_t> m_threads;
+    std::vector<std::thread> m_threads;
     std::vector<std::future<void>> m_futures;
+    bool m_is_finished{false};
 
+    // Access methods
     std::optional<std::string> get_next_token(const std::string_view &);
+
+    // Functions
     void lexer_thread(const std::string_view &);
     void handle_token(const std::string_view &);
 };
