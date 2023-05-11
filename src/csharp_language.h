868875cd534b53d8f002ee0172fb9fc7bc4cd547
Sergio <sgarnica1902@gmail.com>
2023-05-10 18:02:52 -0600

    Restore main

--- src/csharp_language.h
+++ src/csharp_language.h
@@ -14,119 +14,119 @@
 
 namespace csharp
 {
-  // Array data for the tokens (basic)
-  /**
-   * @brief
-   * Array of C# keywords for the lexer
-   * @constexpr std::array<const char *, 77> keywords
-   */
-  constexpr std::array<const char *, 77> m_keywords = {
-      "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while"};
+    // Array data for the tokens (basic)
+    /**
+     * @brief
+     * Array of C# keywords for the lexer
+     * @constexpr std::array<const char *, 77> keywords
+     */
+    constexpr std::array<const char *, 77> m_keywords = {
+        "abstract", "as", "base", "bool", "break", "byte", "case", "catch", "char", "checked", "class", "const", "continue", "decimal", "default", "delegate", "do", "double", "else", "enum", "event", "explicit", "extern", "false", "finally", "fixed", "float", "for", "foreach", "goto", "if", "implicit", "in", "int", "interface", "internal", "is", "lock", "long", "namespace", "new", "null", "object", "operator", "out", "override", "params", "private", "protected", "public", "readonly", "ref", "return", "sbyte", "sealed", "short", "sizeof", "stackalloc", "static", "string", "struct", "switch", "this", "throw", "true", "try", "typeof", "uint", "ulong", "unchecked", "unsafe", "ushort", "using", "virtual", "void", "volatile", "while"};
 
-  /**
-   * @brief
-   * Array of C# operators for the lexer
-   * @constexpr std::array<const char *, 35> operators
-   */
-  constexpr std::array<const char *, 38> m_operators = {
-      "+", "-", "*", "/", "%", "++", "--", "+=", "-=", "*=", "/=", "%=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>", ">>=", "<<=", "&=", "|=", "^=", "??", "=>", "is", "as", ":", "::"};
+    /**
+     * @brief
+     * Array of C# operators for the lexer
+     * @constexpr std::array<const char *, 35> operators
+     */
+    constexpr std::array<const char *, 40> m_operators = {
+        "+", "-", "*", "/", "%", "++", "--", "+=", "-=", "*=", "/=", "%=", "==", "!=", ">", "<", ">=", "<=", "&&", "||", "!", "&", "|", "^", "~", "<<", ">>", ">>=", "<<=", "&=", "|=", "^=", "??", "=>", "is", "as", ":", "::"};
 
-  /**
-   * @brief
-   * Array of C# separators for the lexer
-   * @constexpr std::array<const char *, 12> separators
-   */
-  constexpr std::array<const char *, 12> m_separators = {
-      "(", ")", "{", "}", "[", "]", ",", ";", ":", "?", ".", "::"};
+    /**
+     * @brief
+     * Array of C# separators for the lexer
+     * @constexpr std::array<const char *, 12> separators
+     */
+    constexpr std::array<const char *, 12> m_separators = {
+        "(", ")", "{", "}", "[", "]", ",", ";", ":", "?", ".", "::"};
 
-  /**
-   * @brief
-   * Array of C# comments for the lexer
-   * @constexpr std::array<const char *, 3> comments
-   * @note The last element is an empty string to avoid a bug in the lexer
-   */
-  constexpr std::array<const char *, 4> m_comments = {
-      "//", "/*", "*/", ""};
+    /**
+     * @brief
+     * Array of C# comments for the lexer
+     * @constexpr std::array<const char *, 3> comments
+     * @note The last element is an empty string to avoid a bug in the lexer
+     */
+    constexpr std::array<const char *, 4> m_comments = {
+        "//", "/*", "*/", ""};
 
-  /**
-   * @brief
-   * Array of C# literals for the lexer
-   * @constexpr std::array<const char *, 5> literals
-   */
-  constexpr std::array<const char *, 5> m_literals = {
-      "true", "false", "null", "this", "base"};
+    /**
+     * @brief
+     * Array of C# literals for the lexer
+     * @constexpr std::array<const char *, 5> literals
+     */
+    constexpr std::array<const char *, 5> m_literals = {
+        "true", "false", "null", "this", "base"};
 
-  /**
-   * @brief
-   * Array of C# preprocessor directives for the lexer
-   * @constexpr std::array<const char *, 12> preprocessor_directives
-   */
-  constexpr std::array<const char *, 12> m_preprocessor = {
-      "#define", "#undef", "#if", "#ifdef", "#ifndef", "#else", "#elif", "#endif",
-      "#error", "#warning", "#line", "#pragma"};
+    /**
+     * @brief
+     * Array of C# preprocessor directives for the lexer
+     * @constexpr std::array<const char *, 12> preprocessor_directives
+     */
+    constexpr std::array<const char *, 12> m_preprocessor = {
+        "#define", "#undef", "#if", "#ifdef", "#ifndef", "#else", "#elif", "#endif",
+        "#error", "#warning", "#line", "#pragma"};
 
-  /**
-   * @brief
-   * Array of C# contextual keywords for the lexer
-   * @constexpr std::array<const char *, 3> contextual_keywords
-   */
-  constexpr std::array<const char *, 3> m_contextual_keywords = {
-      "var", "nameof", "yield"};
+    /**
+     * @brief
+     * Array of C# contextual keywords for the lexer
+     * @constexpr std::array<const char *, 3> contextual_keywords
+     */
+    constexpr std::array<const char *, 3> m_contextual_keywords = {
+        "var", "nameof", "yield"};
 
-  // Array data for the tokens (advanced)
-  /**
-   * @brief
-   * Array of C# access specifiers for the lexer
-   * @constexpr std::array<const char *, 6> access_specifiers
-   */
-  constexpr std::array<const char *, 6> m_access_specifiers = {
-      "public", "private", "protected", "internal", "protected internal", "private protected"};
+    // Array data for the tokens (advanced)
+    /**
+     * @brief
+     * Array of C# access specifiers for the lexer
+     * @constexpr std::array<const char *, 6> access_specifiers
+     */
+    constexpr std::array<const char *, 6> m_access_specifiers = {
+        "public", "private", "protected", "internal", "protected internal", "private protected"};
 
-  /**
-   * @brief
-   * Array of C# attribute targets for the lexer
-   * @constexpr std::array<const char *, 4> attribute_targets
-   */
-  constexpr std::array<const char *, 4> m_attribute_targets = {
-      "assembly", "field", "method", "return"};
+    /**
+     * @brief
+     * Array of C# attribute targets for the lexer
+     * @constexpr std::array<const char *, 4> attribute_targets
+     */
+    constexpr std::array<const char *, 4> m_attribute_targets = {
+        "assembly", "field", "method", "return"};
 
-  /**
-   * @brief
-   * Array of C# attribute usages for the lexer
-   * @constexpr std::array<const char *, 3> attribute_usages
-   */
-  constexpr std::array<const char *, 3> m_attribute_usage = {
-      "attribute", "extern", "assembly"};
+    /**
+     * @brief
+     * Array of C# attribute usages for the lexer
+     * @constexpr std::array<const char *, 3> attribute_usages
+     */
+    constexpr std::array<const char *, 3> m_attribute_usage = {
+        "attribute", "extern", "assembly"};
 
-  /**
-   * @brief
-   * Array of C# escaped identifiers for the lexer
-   * @constexpr std::array<const char *, 2> escaped_identifiers
-   */
-  constexpr std::array<const char *, 2> m_escaped_identifiers = {
-      "@", "__arglist"};
+    /**
+     * @brief
+     * Array of C# escaped identifiers for the lexer
+     * @constexpr std::array<const char *, 2> escaped_identifiers
+     */
+    constexpr std::array<const char *, 2> m_escaped_identifiers = {
+        "@", "__arglist"};
 
-  /**
-   * @brief
-   * Array of C# interpolated strings for the lexer
-   * @constexpr std::array<const char *, 2> interpolated_strings
-   */
-  constexpr std::array<const char *, 2> m_interpolated_strings = {
-      "$", "@$"};
+    /**
+     * @brief
+     * Array of C# interpolated strings for the lexer
+     * @constexpr std::array<const char *, 2> interpolated_strings
+     */
+    constexpr std::array<const char *, 2> m_interpolated_strings = {
+        "$", "@$"};
 
-  /**
-   * @brief
-   * Array of C# nullables for the lexer
-   * @constexpr std::array<const char *, 2> nullables
-   */
-  constexpr std::array<const char *, 2> m_nullables = {
-      "?", "??"};
+    /**
+     * @brief
+     * Array of C# nullables for the lexer
+     * @constexpr std::array<const char *, 2> nullables
+     */
+    constexpr std::array<const char *, 2> m_nullables = {
+        "?", "??"};
 
-  /**
-   * @brief
-   * Array of C# preprocessor directives for the lexer
-   * @constexpr std::array<const char *, 12> preprocessor_directives
-   */
-  constexpr std::array<const char *, 2> m_verbatim_strings = {
-      "@", "@\"\""};
+    /**
+     * @brief
+     * Array of C# preprocessor directives for the lexer
+     * @constexpr std::array<const char *, 12> preprocessor_directives
+     */
+    constexpr std::array<const char *, 2> m_verbatim_strings = {
+        "@", "@\"\""};
 }
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -9,9 +9,7 @@
  * @copyright Copyright 2023
  */
 
-#include <algorithm>
 #include "lexer.h"
-#include "token.cpp"
 
 // Methods
 /**
@@ -23,11 +21,11 @@
 std::vector<Token> Lexer::get_tokens_from_file(
     const std::string_view &filename)
 {
-  m_tokens.clear();
-  lex_file(filename);
+    m_tokens.clear();
+    lex_file(filename);
 
-  // Return the tokens
-  return m_tokens;
+    // Return the tokens
+    return m_tokens;
 }
 
 /**
@@ -40,24 +38,24 @@ std::vector<Token> Lexer::get_tokens_from_file(
  */
 void Lexer::lex_files(const std::vector<std::string> &files)
 {
-  try
-  {
-    // Create the threads
-    std::vector<std::thread> threads;
-    threads.reserve(files.size());
+    try
+    {
+        // Create the threads
+        std::vector<std::thread> threads;
+        threads.reserve(files.size());
 
-    for (const auto &file : files)
-      threads.emplace_back(&Lexer::lex_file, this, file);
+        for (const auto &file : files)
+            threads.emplace_back(&Lexer::lex_file, this, file);
 
-    // Wait for the threads to finish
-    for (auto &thread : threads)
-      thread.join();
-  }
-  catch (std::exception &e)
-  {
-    std::cerr << "Error while lexing files: " << e.what()
-              << std::endl;
-  }
+        // Wait for the threads to finish
+        for (auto &thread : threads)
+            thread.join();
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error while lexing files: " << e.what()
+                  << std::endl;
+    }
 }
 
 // Private (methods)
@@ -69,39 +67,39 @@ void Lexer::lex_files(const std::vector<std::string> &files)
  */
 void Lexer::lex_file(const std::string_view &filename)
 {
-  try
-  {
-    std::ifstream file(std::string(filename),
-                       std::ios::in | std::ios::binary);
-
-    if (!file)
-      throw std::runtime_error("Unable to open the file" +
-                               std::string(filename));
-
-    std::ostringstream content_stream;
-    content_stream << file.rdbuf();
-    file.close();
-
-    std::string content = content_stream.str();
-    auto tokens = tokenize(content);
-
-    // Identifying the tokens
-    for (auto &token : tokens)
+    try
     {
-      TokenType token_type = identify_token(token.get_value());
-      token.set_type(token_type);
-    }
+        std::ifstream file(std::string(filename),
+                           std::ios::in | std::ios::binary);
 
-    // Store the tokens
-    std::lock_guard<std::mutex> lock(m_mutex);
-    m_tokens.insert(m_tokens.end(), tokens.begin(),
-                    tokens.end());
-  }
-  catch (std::exception &e)
-  {
-    std::cerr << "Error while lexing file " << filename << ": "
-              << e.what() << std::endl;
-  }
+        if (!file)
+            throw std::runtime_error("Unable to open the file" +
+                                     std::string(filename));
+
+        std::ostringstream content_stream;
+        content_stream << file.rdbuf();
+        file.close();
+
+        std::string content = content_stream.str();
+        auto tokens = tokenize(content);
+
+        // Identifying the tokens
+        for (auto &token : tokens)
+        {
+            TokenType token_type = identify_token(token.get_value());
+            token.set_type(token_type);
+        }
+
+        // Store the tokens
+        std::lock_guard<std::mutex> lock(m_mutex);
+        m_tokens.insert(m_tokens.end(), tokens.begin(),
+                        tokens.end());
+    }
+    catch (std::exception &e)
+    {
+        std::cerr << "Error while lexing file " << filename << ": "
+                  << e.what() << std::endl;
+    }
 }
 
 /**
@@ -112,42 +110,42 @@ void Lexer::lex_file(const std::string_view &filename)
  */
 std::vector<Token> Lexer::tokenize(const std::string_view &content)
 {
-  std::vector<Token> tokens;
-  std::string token;
+    std::vector<Token> tokens;
+    std::string token;
 
-  for (auto &character : content)
-  {
-    if (std::isspace(character))
+    for (auto &character : content)
     {
-      if (!token.empty())
-      {
-        tokens.emplace_back(token);
-        token.clear();
-      }
-    }
-    else if (std::ispunct(character))
-    {
-      if (!token.empty() && !std::ispunct(token[0]))
-      {
-        tokens.emplace_back(token);
-        token.clear();
-      }
+        if (std::isspace(character))
+        {
+            if (!token.empty())
+            {
+                tokens.emplace_back(token);
+                token.clear();
+            }
+        }
+        else if (std::ispunct(character))
+        {
+            if (!token.empty() && !std::ispunct(token[0]))
+            {
+                tokens.emplace_back(token);
+                token.clear();
+            }
 
-      token += character;
+            token += character;
 
-      if (!token.empty())
-      {
-        tokens.emplace_back(token);
-        token.clear();
-      }
+            if (!token.empty())
+            {
+                tokens.emplace_back(token);
+                token.clear();
+            }
+        }
+        else
+        {
+            token += character;
+        }
     }
-    else
-    {
-      token += character;
-    }
-  }
 
-  return tokens;
+    return tokens;
 }
 
 /**
@@ -158,75 +156,75 @@ std::vector<Token> Lexer::tokenize(const std::string_view &content)
  */
 TokenType Lexer::identify_token(const std::string_view &token)
 {
-  if (std::find(csharp::m_keywords.begin(),
-                csharp::m_keywords.end(), token) !=
-      csharp::m_keywords.end())
-    return TokenType::Keyword;
+    if (std::find(csharp::m_keywords.begin(),
+                  csharp::m_keywords.end(), token) !=
+        csharp::m_keywords.end())
+        return TokenType::Keyword;
 
-  if (std::find(csharp::m_literals.begin(),
-                csharp::m_literals.end(), token) !=
-      csharp::m_literals.end())
-    return TokenType::NumericLiteral;
+    if (std::find(csharp::m_literals.begin(),
+                  csharp::m_literals.end(), token) !=
+        csharp::m_literals.end())
+        return TokenType::NumericLiteral;
 
-  if (std::find(csharp::m_operators.begin(),
-                csharp::m_operators.end(), token) !=
-      csharp::m_operators.end())
-    return TokenType::Operator;
+    if (std::find(csharp::m_operators.begin(),
+                  csharp::m_operators.end(), token) !=
+        csharp::m_operators.end())
+        return TokenType::Operator;
 
-  if (std::find(csharp::m_separators.begin(),
-                csharp::m_separators.end(), token) !=
-      csharp::m_separators.end())
-    return TokenType::Separator;
+    if (std::find(csharp::m_separators.begin(),
+                  csharp::m_separators.end(), token) !=
+        csharp::m_separators.end())
+        return TokenType::Separator;
 
-  if (std::find(csharp::m_comments.begin(),
-                csharp::m_comments.end(), token) !=
-      csharp::m_comments.end())
-    return TokenType::Comment;
+    if (std::find(csharp::m_comments.begin(),
+                  csharp::m_comments.end(), token) !=
+        csharp::m_comments.end())
+        return TokenType::Comment;
 
-  if (std::find(csharp::m_preprocessor.begin(),
-                csharp::m_preprocessor.end(), token) !=
-      csharp::m_preprocessor.end())
-    return TokenType::Preprocessor;
+    if (std::find(csharp::m_preprocessor.begin(),
+                  csharp::m_preprocessor.end(), token) !=
+        csharp::m_preprocessor.end())
+        return TokenType::Preprocessor;
 
-  if (std::find(csharp::m_contextual_keywords.begin(),
-                csharp::m_contextual_keywords.end(), token) !=
-      csharp::m_contextual_keywords.end())
-    return TokenType::ContextualKeyword;
+    if (std::find(csharp::m_contextual_keywords.begin(),
+                  csharp::m_contextual_keywords.end(), token) !=
+        csharp::m_contextual_keywords.end())
+        return TokenType::ContextualKeyword;
 
-  if (std::find(csharp::m_access_specifiers.begin(),
-                csharp::m_access_specifiers.end(), token) !=
-      csharp::m_access_specifiers.end())
-    return TokenType::AccessSpecifier;
+    if (std::find(csharp::m_access_specifiers.begin(),
+                  csharp::m_access_specifiers.end(), token) !=
+        csharp::m_access_specifiers.end())
+        return TokenType::AccessSpecifier;
 
-  if (std::find(csharp::m_attribute_targets.begin(),
-                csharp::m_attribute_targets.end(), token) !=
-      csharp::m_attribute_targets.end())
-    return TokenType::AttributeTarget;
+    if (std::find(csharp::m_attribute_targets.begin(),
+                  csharp::m_attribute_targets.end(), token) !=
+        csharp::m_attribute_targets.end())
+        return TokenType::AttributeTarget;
 
-  if (std::find(csharp::m_attribute_usage.begin(),
-                csharp::m_attribute_usage.end(), token) !=
-      csharp::m_attribute_usage.end())
-    return TokenType::AttributeUsage;
+    if (std::find(csharp::m_attribute_usage.begin(),
+                  csharp::m_attribute_usage.end(), token) !=
+        csharp::m_attribute_usage.end())
+        return TokenType::AttributeUsage;
 
-  if (std::find(csharp::m_escaped_identifiers.begin(),
-                csharp::m_escaped_identifiers.end(), token) !=
-      csharp::m_escaped_identifiers.end())
-    return TokenType::EscapedIdentifier;
+    if (std::find(csharp::m_escaped_identifiers.begin(),
+                  csharp::m_escaped_identifiers.end(), token) !=
+        csharp::m_escaped_identifiers.end())
+        return TokenType::EscapedIdentifier;
 
-  if (std::find(csharp::m_interpolated_strings.begin(),
-                csharp::m_interpolated_strings.end(), token) !=
-      csharp::m_interpolated_strings.end())
-    return TokenType::InterpolatedStringLiteral;
+    if (std::find(csharp::m_interpolated_strings.begin(),
+                  csharp::m_interpolated_strings.end(), token) !=
+        csharp::m_interpolated_strings.end())
+        return TokenType::InterpolatedStringLiteral;
 
-  if (std::find(csharp::m_nullables.begin(),
-                csharp::m_nullables.end(), token) !=
-      csharp::m_nullables.end())
-    return TokenType::NullLiteral;
+    if (std::find(csharp::m_nullables.begin(),
+                  csharp::m_nullables.end(), token) !=
+        csharp::m_nullables.end())
+        return TokenType::NullLiteral;
 
-  if (token[0] == '@')
-    return TokenType::VerbatimStringLiteral;
+    if (token[0] == '@')
+        return TokenType::VerbatimStringLiteral;
 
-  return TokenType::Identifier;
+    return TokenType::Identifier;
 }
 
 /**
@@ -237,74 +235,74 @@ TokenType Lexer::identify_token(const std::string_view &token)
  */
 std::string token_to_html(const Token &token)
 {
-  std::string html = "<span class=\"";
-  switch (token.get_type())
-  {
-  case TokenType::Keyword:
-    html += "keyword";
-    break;
+    std::string html = "<span class=\"";
+    switch (token.get_type())
+    {
+    case TokenType::Keyword:
+        html += "keyword";
+        break;
 
-  case TokenType::Identifier:
-    html += "identifier";
-    break;
+    case TokenType::Identifier:
+        html += "identifier";
+        break;
 
-  case TokenType::NumericLiteral:
-    html += "literal";
-    break;
+    case TokenType::NumericLiteral:
+        html += "literal";
+        break;
 
-  case TokenType::Operator:
-    html += "operator";
-    break;
+    case TokenType::Operator:
+        html += "operator";
+        break;
 
-  case TokenType::Separator:
-    html += "separator";
-    break;
+    case TokenType::Separator:
+        html += "separator";
+        break;
 
-  case TokenType::Comment:
-    html += "comment";
-    break;
+    case TokenType::Comment:
+        html += "comment";
+        break;
 
-  case TokenType::Preprocessor:
-    html += "preprocessor";
-    break;
+    case TokenType::Preprocessor:
+        html += "preprocessor";
+        break;
 
-  case TokenType::ContextualKeyword:
-    html += "contextual-keyword";
-    break;
+    case TokenType::ContextualKeyword:
+        html += "contextual-keyword";
+        break;
 
-  case TokenType::AccessSpecifier:
-    html += "access-specifier";
-    break;
+    case TokenType::AccessSpecifier:
+        html += "access-specifier";
+        break;
 
-  case TokenType::AttributeTarget:
-    html += "attribute-target";
-    break;
+    case TokenType::AttributeTarget:
+        html += "attribute-target";
+        break;
 
-  case TokenType::AttributeUsage:
-    html += "attribute-usage";
-    break;
+    case TokenType::AttributeUsage:
+        html += "attribute-usage";
+        break;
 
-  case TokenType::EscapedIdentifier:
-    html += "escaped-identifier";
-    break;
+    case TokenType::EscapedIdentifier:
+        html += "escaped-identifier";
+        break;
 
-  case TokenType::InterpolatedStringLiteral:
-    html += "interpolated-string-literal";
-    break;
+    case TokenType::InterpolatedStringLiteral:
+        html += "interpolated-string-literal";
+        break;
 
-  case TokenType::NullLiteral:
-    html += "null-literal";
-    break;
+    case TokenType::NullLiteral:
+        html += "null-literal";
+        break;
 
-  case TokenType::VerbatimStringLiteral:
-    html += "verbatim-string-literal";
-    break;
+    case TokenType::VerbatimStringLiteral:
+        html += "verbatim-string-literal";
+        break;
 
-  default:
-    html += "other";
-    break;
-  }
+    default:
+        html += "other";
+        break;
+    }
 
-  html += "\">" + token.get_value() + "</span>";
-  return html;
+    html += "\">" + token.get_value() + "</span>";
+    return html;
 }
